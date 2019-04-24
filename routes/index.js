2db173321c94e2dcead5d0fcf13e51a02071aa16
Imran Khan <Imran.khan@synergy.it>
2019-04-25 00:10:34 +0500

    read the xml file

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var Parser = require("xml-node").XmlParser;
 var fs =require('fs'); 
 var convert = require('xml-js');
 var xml = require('xml');
+let listOfComments = [];
 
 
 
@@ -14,14 +15,44 @@ router.get('/', function(req, res, next) {
   fs.readFile( filePath, 'utf8', function(err, data) {
     //var result = convert.xml2json(data, {compact: true, spaces: 4});
     //res.json(result);
-    // parseString(data, function (err, result) {
-    //   console.log(result);
-    //   //res.json(result);
-    //     });
-    res.set('Content-Type', 'text/plain');
-    res.send(xml(data));
+    parseString(data, function (err, result) {
+      //console.log(result.report.assessment[0].node[0].$.code);
+      //console.log(result.report.assessment[0].node.length);
+      for(let i=0; i<result.report.assessment[0].node.length; i++){
+        if(result.report.assessment[0].node[i].$.code == "generic"){
+          console.log(result.report.assessment[0].node[i].node.length);
+          for(let j =0; j<result.report.assessment[0].node[i].node.length; j++){
+            if(result.report.assessment[0].node[i].node[j].$['comment-date'])
+            console.log(result.report.assessment[0].node[i].node[j].$['comment-date']);
+            if(result.report.assessment[0].node[i].node[j].node){
+              for(let k = 0; k<result.report.assessment[0].node[i].node[j].node.length; k++){
+                if(result.report.assessment[0].node[i].node[j].node[k].$['comment-date'])
+                {
+                  let commentData = {
+                    commentDate : '',
+                    comment : ''
+                  }
+                  commentData.commentDate = result.report.assessment[0].node[i].node[j].node[k].$['comment-date'];
+                  commentData.comment = result.report.assessment[0].node[i].node[j].node[k].$.comment;
+                  listOfComments.push(commentData)
+                }
+        
+              }
+            }
+          }
+
+
+        }
+      }
+      console.log(listOfComments);
+      res.render('index', { listOfComments: listOfComments });
+      //res.json(result);
+        });
+    // res.set('Content-Type', 'text/plain');
+    // res.send(xml(data));
   });
-  //res.render('index', { title: 'Express' });
+  // console.log(listOfComments);
+  // res.render('index', { listOfComments: listOfComments });
 });
 
 router.get('/facebook', function(req, res, next) {
